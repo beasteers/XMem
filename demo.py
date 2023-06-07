7ec03b69c00c5c9dbe036ca5ebb3b16b28fd9855
Bea Steers <bea.steers@gmail.com>
2023-06-07 13:18:54 -0400

    cleanup demo and bug fixes

--- demo.py
+++ demo.py
@@ -14,7 +14,7 @@ from xmem.inference import InferenceCore, XMem
 from xmem.inference.interact.interactive_utils import image_to_torch, torch_prob_to_numpy_mask, overlay_davis
 from torchvision.ops import masks_to_boxes
 import deep_sort
-
+from detic.inference import Detic
 
 
 device = 'cuda'
@@ -34,25 +34,22 @@ xmem_config = {
 
 
 @torch.no_grad()
-def main(src, vocab='lvis', detect_every=1, skip_frames=30, fps_down=1, size=480):
-    from detic.inference import Detic
-
-    # NOTE: xmem rounds image sizes to a factor of 16
-    WH = (int(720/480*size)//16*16, int(size)//16*16)
-
+def main(src, vocab='lvis', detect_every=1, skip_frames=0, fps_down=1, size=480):
     # object detector
-    detic_model = Detic(['cat'], conf_threshold=0.5, masks=True).to(device)
-    detic_model.set_vocab(vocab)
+    detic_model = Detic(vocab, conf_threshold=0.5, masks=True).to(device)
     print(detic_model.labels)
 
     # box tracker
     ds_tracker = deep_sort.Tracker(deep_sort.NearestNeighbor('cosine', 0.2), max_age=None)
+    # ds_tracker.tracks is a dictionary of object_id -> Track
+    # see: https://github.com/VIDA-NYU/deep_sort/blob/master/deep_sort/track.py
 
     # mask tracker
     network = XMem(xmem_config).eval().to(device)
     processor = InferenceCore(network, config=xmem_config)
 
     # object label counter (dict of object_track_id -> {plate: 25, cutting_board: 1})
+    # this should have the same keys as ds_tracker
     object_labels = defaultdict(lambda: Counter())
 
     # a kernel to make tiny segmentatation maps a bit bigger (e.g. toothpicks)
@@ -60,8 +57,15 @@ def main(src, vocab='lvis', detect_every=1, skip_frames=30, fps_down=1, size=480
 
     # get the source video info
     video_info = sv.VideoInfo.from_video_path(video_path=src)
-    video_info.width, video_info.height = WH
+    # make the video size a multiple of 16 (because otherwise it won't generate masks of the right size)
+    aspect = video_info.width / video_info.height
+    video_info.width = int(aspect*size)//16*16
+    video_info.height = int(size)//16*16
+    WH = video_info.width, video_info.height
+
+    # double width because we have both detic and xmem frames
     video_info.width *= 2
+    # possibly reduce the video frame rate
     og_fps = video_info.fps
     video_info.fps /= fps_down
     t_obs = 0
@@ -85,23 +89,25 @@ def main(src, vocab='lvis', detect_every=1, skip_frames=30, fps_down=1, size=480
 
             labels = mask = None
             if not i % int(detect_every*og_fps) or i == skip_frames:
+                # predict objects
                 outputs = detic_model(frame)["instances"]
                 mask = outputs.pred_masks.int()
+
+                # create the output frame for detic and overlay the object masks
                 mask1=torch.argmax(torch.cat([torch.zeros(1, *mask.shape[1:]), mask.cpu()]), dim=0)
                 out_frame2 = overlay_davis(out_frame.copy(), mask1)
-
+                
+                # get box coordinates, scores, and labels
                 xywh = outputs.pred_boxes.tensor.cpu().numpy()
                 cls_ids = outputs.pred_classes.cpu().numpy()
                 scores = outputs.scores.cpu().numpy()
                 features = outputs.clip_features.cpu().numpy()
                 labels = detic_model.labels[cls_ids]
 
-                # mask_f = mask.float()
-                for i, size in enumerate(mask.sum((1, 2))):
-                    if size < 0.01 * mask.shape[1] * mask.shape[2]:
-                        # print("dilating", labels[i], mask[i].shape)
-                        mask[i] = torch.as_tensor(cv2.dilate(mask[i].cpu().float().numpy(), kernel, iterations=1), device=device)
+                # increase the size of very small masks to make tracking easier
+                mask = dilate_masks(mask, kernel)
 
+                # update deep sort
                 # matches, unmatched_tracks, unmatched_detections = ds_tracker._match([])
                 t_obs += 1
                 ds_tracker.predict(t_obs)
@@ -114,18 +120,17 @@ def main(src, vocab='lvis', detect_every=1, skip_frames=30, fps_down=1, size=480
             # Run object tracking
             ######
 
+            # run xmem
             frame_torch, _ = image_to_torch(frame, device=device)
             prediction, track_ids = processor.step(frame_torch, mask)
 
-            # update deep-sort and label counter with tracking results
+            # update deep-sort and label counter with tracking results only when we have new detic updates
             all_labels = list(processor.all_labels)
             if labels is not None:
-                if track_ids is None:
-                    track_ids = processor.all_labels
-
                 # update matches in deep sort - use deep-sort to delete objects that are bad matches across multiple frames
                 matches = [(ti, di) for di, ti in enumerate(track_ids)]
                 deleted = ds_tracker._update(detections, t_obs, matches)
+                # delete objects that didn't have consistent initial detections
                 deleted and print('deleting', set(deleted))
                 for i in deleted:
                     processor.delete_object_id(i)
@@ -134,6 +139,7 @@ def main(src, vocab='lvis', detect_every=1, skip_frames=30, fps_down=1, size=480
                 for ((ti, di), l) in zip(matches, labels):
                     object_labels[ti].update((l,))
 
+                # draw boxes on the detic frame
                 draw_boxes(out_frame2, xywh, [
                     [f'{i} {l}'] 
                     for i, l in zip(track_ids, detic_model.labels[cls_ids])
@@ -146,9 +152,11 @@ def main(src, vocab='lvis', detect_every=1, skip_frames=30, fps_down=1, size=480
             if len(prediction):
                 # draw mask
                 pred_mask = torch_prob_to_numpy_mask(prediction)
+                if pred_mask.ndim == 3:
+                    pred_mask = pred_mask[0] # ??? I suspect something is wrong with `prediction`
                 out_frame = overlay_davis(out_frame, pred_mask)
 
-                # convert masks to bounding boxes and draw
+                # convert masks to bounding boxes and draw box with label counts
                 if pred_mask.any():
                     ids=np.unique(pred_mask)[1:]
                     ids = [i for i in ids if all_labels[i-1] in ds_tracker.tracks and ds_tracker.tracks[all_labels[i-1]].is_confirmed()]
@@ -163,6 +171,15 @@ def main(src, vocab='lvis', detect_every=1, skip_frames=30, fps_down=1, size=480
 
             s.write_frame(np.concatenate([out_frame, out_frame2], axis=1))
 
+
+def dilate_masks(mask, kernel, rel_area=0.01):
+    # dilate masks whose overall area is below a certain value
+    # this makes it easier for xmem to track those objects
+    for i, size in enumerate(mask.sum((1, 2))):
+        if size < rel_area * mask.shape[1] * mask.shape[2]:
+            # print("dilating", labels[i], mask[i].shape)
+            mask[i] = torch.as_tensor(cv2.dilate(mask[i].cpu().float().numpy(), kernel, iterations=1), device=device)
+    return mask
 
 
 # bounding box drawing utils
