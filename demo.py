38b9602d55d9b62af3df08607a44637c3862c897
Bea Steers <bea.steers@gmail.com>
2023-09-19 10:31:02 -0400

    minor demo improvements

--- demo.py
+++ demo.py
@@ -2,9 +2,11 @@
 
 usage: python demo.py ../R0-P03_03.mp4 --vocab '["peanut butter jar", "jelly jar", "plate", "cutting board", "paper towel", "tortilla, white circular flat", "burrito", "toothpicks", "floss", "person", "knife"]' --fps-down 3
 '''
+import os
 import sys
 import time
 import logging
+import contextlib
 from collections import defaultdict, Counter
 from tqdm.contrib.logging import logging_redirect_tqdm
 
@@ -33,7 +35,7 @@ xmem_config = {
     'num_prototypes': 64,
     'min_mid_term_frames': 6,
     'max_mid_term_frames': 12,
-    'max_long_term_elements': 5000,
+    'max_long_term_elements': 1000,
     # 'dilate_size_threshold': 0.01,
     'tentative_frames': 3,
     'max_age': 60 * 30,
@@ -48,7 +50,8 @@ import ipdb
 def mainx(*a, **kw):
     return main(*a, **kw)
 
-def main(src, vocab='lvis', untracked_vocab=None, out_path='xmem_output.mp4', detect_every=1, skip_frames=0, fps_down=1, size=480, limit=None):
+def main(src, vocab='lvis', untracked_vocab=None, out_path=None, detect_every=1, skip_frames=0, fps_down=1, size=480, limit=None):
+    out_path = out_path or f'xmem_{os.path.basename(src)}'
     if untracked_vocab:
         vocab = vocab + untracked_vocab
     
@@ -68,10 +71,11 @@ def main(src, vocab='lvis', untracked_vocab=None, out_path='xmem_output.mp4', de
     # video read-write loop
     video_info, WH = get_video_info(src, size, fps_down, ncols=2)
     afps = video_info.fps / fps_down
-    i_detect = int(detect_every*video_info.og_fps//fps_down)
+    i_detect = int(detect_every*video_info.og_fps//fps_down) or 1
     print('detecting every', i_detect, detect_every, afps, detect_every%(1/afps))
+    detect_out_frame = None 
     
-    with sv.VideoSink(target_path=out_path, video_info=video_info) as s:
+    with sv.VideoSink(out_path, video_info) as s, XMemWriter(out_path, video_info) as tw:
         for i, frame in enumerate(tqdm.tqdm(sv.get_video_frames_generator(src), total=video_info.total_frames)):
             if i < skip_frames or i%fps_down: continue
             if limit and i > limit: break
@@ -80,7 +84,7 @@ def main(src, vocab='lvis', untracked_vocab=None, out_path='xmem_output.mp4', de
 
             # run detic
             detections = det_mask = None
-            if i_detect and (not i % i_detect or detect_out_frame is None):
+            if detect_out_frame is None or not i % i_detect:
                 # get object detections
                 outputs = detic_model(frame)
                 det_mask = outputs["instances"].pred_masks.int()
@@ -90,7 +94,7 @@ def main(src, vocab='lvis', untracked_vocab=None, out_path='xmem_output.mp4', de
                 log.info(f'Detected ({i}|{i_detect}): {detic_model.labels[detections.class_id]}')
 
                 if untracked_vocab:
-                    keep = np.array([l not in untracked_vocab for l in detic_model.labels[detections.class_id]])
+                    keep = np.array([l not in untracked_vocab for l in detic_model.labels[detections.class_id]], dtype=bool)
                     det_mask = det_mask[keep]
                     detections = detections[keep]
                 
@@ -104,13 +108,118 @@ def main(src, vocab='lvis', untracked_vocab=None, out_path='xmem_output.mp4', de
                 for (ti, l) in zip(input_track_ids, detections.class_id):
                     label_counts[ti].update([l])
 
+
             # draw xmem
             track_out_frame, detections = draw.draw_tracks(frame, pred_mask, label_counts, track_ids, detic_model.labels)
+            
+            # write videos for each track
+            tw.write_tracks(frame, detections)
 
             # write frame to file
             s.write_frame(np.concatenate([track_out_frame, detect_out_frame], axis=1))
 
     print("wrote to:", out_path)
+
+
+
+class XMemWriter:
+    def __init__(self, out_path, video_info, size=200, padding=0):
+        # self.sink = sv.VideoSink(target_path=out_path, video_info=video_info)
+        self.track_out_format = '{}_track{{}}{}'.format(*os.path.splitext(out_path))
+        os.makedirs(os.path.dirname(self.track_out_format) or '.', exist_ok=True)
+        self.video_info = sv.VideoInfo(width=size, height=size, fps=video_info.fps)
+        self.size = (self.video_info.height, self.video_info.width)
+        self.padding = padding
+        self.writers = {}
+        self.ba = BoxAnnotator()
+        self.ma = sv.MaskAnnotator()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *a):
+        for w in self.writers.values():
+            w.__exit__(*a)
+        # self.sink.__exit__(*a)
+
+    def write_tracks(self, frame, detections):
+        for tid, bbox in zip(detections.tracker_id, detections.xyxy):
+            if tid not in self.writers:
+                self.writers[tid] = sv.VideoSink(self.track_out_format.format(tid), video_info=self.video_info)
+                self.writers[tid].__enter__()
+            self._write_frame(self.writers[tid], frame, bbox)
+
+    # def write_frame(self, frame, detections):
+    #     frame = self._draw_detections(frame, detections)
+    #     self.sink.write_frame(frame)
+
+    def _draw_detections(self, frame, detections):
+        frame = frame.copy()
+        frame = self.ma.annotate(frame, detections)
+        frame = self.ba.annotate(frame, detections, labels=labels)
+        return frame
+
+    def _write_frame(self, writer, frame=None, bbox=None):
+        if frame is None:
+            frame = np.zeros(self.size, dtype='uint8')
+        elif bbox is not None:
+            x, y, x2, y2 = map(int, bbox)
+            frame = frame[y - self.padding:y2 + self.padding, x - self.padding:x2 + self.padding]
+        frame = resize_with_pad(frame, self.size)
+        writer.write_frame(frame)
+
+
+# class TrackVideoWriter:
+#     def __init__(self, out_path, fps, size=200, padding=0):
+#         self.video_info = sv.VideoInfo(width=size, height=size, fps=fps)
+#         os.makedirs(os.path.dirname(out_path) or '.', exist_ok=True)
+#         self.sink = sv.VideoSink(target_path=out_path, video_info=self.video_info)
+#         self.size = (self.video_info.height, self.video_info.width)
+#         self.padding = padding
+#         self.sink.__enter__()
+    
+#     def write_frame(self, frame=None, bbox=None):
+#         if frame is None:
+#             frame = np.zeros(self.size, dtype='uint8')
+#         elif bbox is not None:
+#             x, y, x2, y2 = map(int, bbox)
+#             frame = frame[y - self.padding:y2 + self.padding, x - self.padding:x2 + self.padding]
+#         frame = resize_with_pad(frame, self.size)
+#         self.sink.write_frame(frame)
+
+#     @classmethod
+#     def write_tracks(cls, writers, frame, track_ids, bboxes, out_dir, fps, size=200):
+#         for tid, bbox in zip(track_ids, bboxes):
+#             if tid not in writers:
+#                 writers[tid] = cls(f'{out_dir}/{tid}.mp4', fps, size)
+#             writers[tid].write_frame(frame, bbox)
+#         # # write black frame if nothing is detected
+#         # for tid in set(writers) - set(track_ids):
+#         #     writers[tid].write_frame()
+
+#     @classmethod
+#     @contextlib.contextmanager
+#     def closing_writers(self, writers):
+#         try:
+#             yield
+#         finally:
+#             import sys
+#             for tid, w in writers.items():
+#                 w.sink.__exit__(*sys.exc_info())
+
+def resize_with_pad(image, new_shape):
+    """Maintains aspect ratio and resizes with padding."""
+    original_shape = (image.shape[1], image.shape[0])
+    if not all(original_shape):
+        return np.zeros(new_shape, dtype=np.uint8)
+    ratio = float(max(new_shape))/max(original_shape)
+    new_size = tuple([int(x*ratio) for x in original_shape])
+    image = cv2.resize(image, new_size)
+    delta_w = new_shape[0] - new_size[0]
+    delta_h = new_shape[1] - new_size[1]
+    top, bottom = delta_h//2, delta_h-(delta_h//2)
+    left, right = delta_w//2, delta_w-(delta_w//2)
+    return cv2.copyMakeBorder(image, top, bottom, left, right, cv2.BORDER_CONSTANT)
 
 
 
